54c71327cf3b394a1bba5fe4d21cdce1cbdefe33
davidauza.engineer <davidauza01@gmail.com>
2020-05-30 18:27:52 -0500

    Add validations to Api::User model

--- app/models/api/user.rb
+++ app/models/api/user.rb
@@ -1,3 +1,10 @@
 class Api::User < ApplicationRecord
   has_secure_password
+  validates :name, length: { minimum: 1, maximum: 255 }, presence: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i.freeze
+  validates :email, format: { with: VALID_EMAIL_REGEX },
+                    length: { minimum: 5, maximum: 255 },
+                    presence: true,
+                    uniqueness: true
+  validates :password, length: { minimum: 6, maximum: 255 }
 end
